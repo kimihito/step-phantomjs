f51b39748a6a1073ad6e16818705caf3d6ed39ca
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-21 02:36:18 +0900

    Fix binary url

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.7-linux-x86_64.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
 tar xfj phantomjs-1.9.7-linux-x86_64.tar.bz2
 sudo cp /tmp/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin
