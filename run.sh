7810f686280dc198bb414bfe85e3de9fba1c3501
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-02-21 02:25:36 +0900

    Update to phantomjs-1.9.7

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
-tar xfj phantomjs-1.9.1-linux-x86_64.tar.bz2
-sudo cp /tmp/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin
+wget https://phantomjs.googlecode.com/files/phantomjs-1.9.7-linux-x86_64.tar.bz2
+tar xfj phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo cp /tmp/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin
