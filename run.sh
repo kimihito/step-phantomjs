ec4192a563401b9f68c3af59fcd56337089538a8
Yoshinori Kawasaki <ykawasaki@gmail.com>
2014-11-05 02:57:11 +0900

    Update to 1.9.8 (to fix POODLE)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar xfj phantomjs-1.9.7-linux-x86_64.tar.bz2
-sudo cp /tmp/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+tar xfj phantomjs-1.9.8-linux-x86_64.tar.bz2
+sudo cp /tmp/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin
